75d48a35e7eb7234d96e18ea2aafcefdb5c0c4e9
gszep <gregory.szep@gmail.com>
2020-12-21 02:22:32 +0100

    first em example successful

--- machine-learning/expectation-maximisation.jl
+++ machine-learning/expectation-maximisation.jl
@@ -11,6 +11,7 @@ begin
 	
 	using LaTeXStrings,Parameters
 	using LinearAlgebra
+	using Zygote
 	
 	using AbstractPlotting.MakieLayout
 	using WGLMakie,AbstractPlotting
@@ -140,12 +141,12 @@ begin
 
 	empty!(scene.events.mousedrag.listeners)
 	mouseevents = addmouseevents!(ax.scene)
-
+	
 	######################################################### model parameters
 	μ₁,μ₂= Node([4.0,5.0]), Node([2.5,8.0])
 	θ = @lift((
 		μ=[ $μ₁, $μ₂ ], π=[ 1/2, 1/2 ],
-		Σ=[ Diagonal([1/3,1/3]), Diagonal([1/3,1/3]) ],
+		Σ=[ [1/3 0; 0 1/3], [1/3 0; 0 1/3] ],
 	))
 	
 	############################################################ marginal
@@ -155,10 +156,10 @@ begin
 		colormap=cgrad(:starrynight, 10, categorical=true),
 		interpolate=true )
 	
-	############################################################# posterior
-	boundary = @lift([ posterior([x,y],[false,true],$θ) for x in xs, y in ys])
-	contour!( ax, xs, ys, boundary, colormap=:grays, levels=[1/4,1/2,3/4] )
+	scatter!( ax, @lift(transpose($μ₁)), marker="1", color=:white )
+	scatter!( ax, @lift(transpose($μ₂)), marker="2", color=:white )
 	
+	############################################################# posterior
 	scatter!( ax, map( (x,y) -> Point(x,y), data.eruptions, data.waiting),
 		markersize=@lift( map( (x,y) -> 5posterior([x,y],[false,true],$θ), 
 				data.eruptions, data.waiting )), color=:gold )
@@ -167,19 +168,140 @@ begin
 		markersize=@lift( map( (x,y) -> 5posterior([x,y],[true,false],$θ), 
 				data.eruptions, data.waiting )), color=:white )
 	
-	###################################################### bind mouse events
-	onmouseleftdown(mouseevents) do event μ₁[] = event.data end
-	onmouserightdown(mouseevents) do event μ₂[] = event.data end
+	###################################################### bind input events	
+	on(scene.events.keyboardbuttons) do button
+		
+		if ispressed(button, Keyboard._1)
+			μ₁[] = mouseevents.obs.val.data
+			
+		elseif ispressed(button, Keyboard._2)
+			μ₂[] = mouseevents.obs.val.data
+		end
+	end
 	
     RecordEvents( scene, "output" )
     scene
 end
 
+# ╔═╡ 8a4a4e48-431c-11eb-07ff-09c8b60bd020
+md"""
+However in practice we are not given dataset ``\mathbf{Z}`` so the best thing we 
+can do estimate what the probability of observing ``\mathbf{z}`` is given that we 
+know ``\mathbf{x}``. This is precisely what we do in the expectation step, when 
+we evaluate the posterior likelihood ``p(\mathbf{z}|\mathbf{x},\theta)``
+"""
+
 # ╔═╡ 01ac872a-4113-11eb-1ffa-f3087641bcf6
 md"""
-However in practice we are not given dataset ``\mathbf{Z}`` so the best thing we can do estimate what the probability of observing ``\mathbf{z}`` is given that we know ``\mathbf{x}``. This is precisely what we do in the expectation step, when we evaluate the posterior likelihood ``p(\mathbf{Z}|\mathbf{X},\theta)``
+Let's look at this posterior for the old faithful dataset which has dimensions 
+``N=2`` and seems to have ``K=2`` mixtures. We shall initialise a random 
+``\theta`` and plot the contours of gaussian mixtures ``p(\mathbf{x}|\theta)``. 
+**You can change the locations of the two mixtures my clicking on the plot, then 
+press 1 or 2 on your keyboard when you would like to place the first or second 
+mixture.**
+"""
 
-Let's look at this posterior for the old faithful dataset which has dimensions ``N=2`` and seems to have ``K=2`` mixtures. We shall initialise a random ``\theta`` and plot the contours of marginal ``p(\mathbf{x}|\theta)``. We shall visualise the likelihood of each datum ``p(\mathbf{z}|\mathbf{x},\theta)`` as a colour
+# ╔═╡ 2d939648-431c-11eb-16c4-136bd9e12d8e
+md"""
+We shall visualise the likelihood of each data point ``p(\mathbf{z}|\mathbf{x},\theta)`` as a colour: white if the probability of the first mixture ``\mathbf{z}=(1,0)`` is greater than the probability of the second ``\mathbf{z}=(0,1)`` and gold otherwise.
+"""
+
+# ╔═╡ cbc6114a-4314-11eb-1593-f588436f287c
+md"""
+We can see that the closer a data point ``\mathbf{x}`` is to mixture ``k``, the higher the probability ``p(\mathbf{z}|\mathbf{x},\theta)`` such that ``\mathbf{z}`` is a one-hot vector where the ``k``-th component is non-zero. How do we design an iterative proceedure to bring the mixtures to overlap with the high data density regions?
+
+The evaluation of posterior likelihood ``p(\mathbf{z}|\mathbf{x},\theta)`` is known as the **expectation** step. What mixture do we expect each data point to belong to? In general, what do we expect the values of our latent variables ``\mathbf{z}`` to be given that we have ``\mathbf{x}``?
+
+The **maximisation** step involves maximising the joint log likelihood ``\ln p(\mathbf{x},\mathbf{z}|\theta')`` as we would in the usual maximum likelihood approach, but here we take into account the expected values ``\mathbf{z}``. This can be done by averaging over ``\mathbf{Z}`` and since we have ``p(\mathbf{z}|\mathbf{x},\theta)`` we can write this as
+"""
+
+# ╔═╡ 95544938-4319-11eb-339b-e9349790aea3
+L"
+Q(\theta,\theta'):=\sum_{\mathbf{z}} p(\mathbf{z}|\mathbf{x},\theta) \ln p(\mathbf{x},\mathbf{z}|\theta')
+"
+
+# ╔═╡ 9009b516-431a-11eb-17ce-998e61d11549
+function Q(θ::NamedTuple,θ′::NamedTuple,x::Vector{<:Number})
+	Z = [[true,false],[false,true]] # specific for two mixtures
+	return sum( z->posterior(x,z,θ)*log(joint(x,z,θ′)), Z )
+end
+
+# ╔═╡ aa8e98e2-4320-11eb-267d-ddd980a07d3f
+md"
+Since likelihoods factorise with respect whole datasets ``\mathbf{X}``, leading to sums in log likelihoods, the function ``Q(\theta,\theta')`` can be evaluated as a sum over the whole dataset
+"
+
+# ╔═╡ efb77868-431f-11eb-2173-8bb0a2cf97c9
+function Q(θ::NamedTuple,θ′::NamedTuple,X::Vector{<:Vector})
+	return sum( x->Q(θ,θ′,x), X )
+end
+
+# ╔═╡ e33547b0-4319-11eb-1f54-ff0d5101b85c
+md"
+We would maximise this function with respect to an unknown ``\theta'`` keeping the initial ``\theta`` we used for the expectation step fixed. Once the maximum has been found for some optimal ``\theta'`` we can update the parameter ``\theta\leftarrow\theta'`` and repeat the proceedure.
+"
+
+# ╔═╡ db2cd3b0-431b-11eb-0635-0d71b8e3ab29
+begin
+	
+	############################# initialise dataset
+	X = map( (x,y)->[x,y], data.eruptions, data.waiting )
+	
+	############################# initial parameters
+	θₜ = ( μ=[ [4.0,5.0], [2.5,8.0] ], π=[ 1/2, 1/2 ],
+		   Σ=[ [1/3 0; 0 1/3], [1/3 0; 0 1/3] ] )
+	
+	η = 10^-3 ###### learning rate
+	for _ ∈ 1:40 ### algorithm iterations
+
+		######################## expectaction-maximisation
+		∂θ, = gradient( θ -> Q(θₜ,θ,X), θₜ )
+
+		###################### update parameters
+		θₜ.μ .= θₜ.μ + η*∂θ.μ
+		θₜ.Σ .= θₜ.Σ + η*∂θ.Σ
+		θₜ.π .= θₜ.π + η*∂θ.π
+
+		###################### enforce constraints
+		θₜ.π ./= sum(θₜ.π)
+	end
+end
+
+# ╔═╡ 49de5dc2-4328-11eb-3352-57e88bddb934
+begin
+	###################################################### scene construction
+	scene₁, layout₁ = layoutscene(resolution = (500,500))
+	ax₁ = layout₁[1,1] = LAxis(scene₁,
+		
+		xpanlock = true, xzoomlock = true,
+		ypanlock = true, yzoomlock = true,
+		
+		xlabel="Eruptions", ylabel="Waiting")
+	empty!(scene₁.events.mousedrag.listeners)
+	
+	############################################################ marginal
+	heatmap!( ax₁, xs, ys, [ marginal([x,y],[false,true],θₜ) for x in xs, y in ys],
+		colormap=cgrad(:starrynight, 10, categorical=true),
+		interpolate=true )
+	
+	############################################################# posterior
+	scatter!( ax₁, map( (x,y) -> Point(x,y), data.eruptions, data.waiting),
+		markersize= map( (x,y) -> 5posterior([x,y],[false,true],θₜ), 
+				data.eruptions, data.waiting ), color=:gold )
+	
+	scatter!( ax₁, map( (x,y) -> Point(x,y), data.eruptions, data.waiting),
+		markersize= map( (x,y) -> 5posterior([x,y],[true,false],θₜ), 
+				data.eruptions, data.waiting ), color=:white )
+	
+    RecordEvents( scene₁, "output" )
+    scene₁
+end
+
+# ╔═╡ e88e67b6-4317-11eb-1a9f-3b7864692294
+md"""
+In fact ``K``-means would suffice for this task so why are we bothing with this more complicated approach? Rather than assigning each data point to a cluster centroid like in  ``K``-means, each data point has a probability of belonging to a mixture ``p(\mathbf{z}|\mathbf{x},\theta)`` . 
+
+Furthermore we are not limited the gaussian mixtures and can consider more complicated distributions ``p(\mathbf{x}|\mathbf{z},\theta)``. Finally we can consider the problem of latent variables in general, where ``\mathbf{z}`` is not just a one-hot encoding of a mixture.
 """
 
 # ╔═╡ Cell order:
@@ -201,5 +323,16 @@ Let's look at this posterior for the old faithful dataset which has dimensions `
 # ╟─d497c0f0-4115-11eb-1f01-df9a4d33dd08
 # ╠═0f49f918-4090-11eb-3865-1771ff1ecb70
 # ╠═b159cec8-408a-11eb-3728-0d266078dfa3
+# ╟─8a4a4e48-431c-11eb-07ff-09c8b60bd020
 # ╟─01ac872a-4113-11eb-1ffa-f3087641bcf6
+# ╟─2d939648-431c-11eb-16c4-136bd9e12d8e
 # ╟─ee8ca6b6-4121-11eb-2685-1bc42b156926
+# ╟─cbc6114a-4314-11eb-1593-f588436f287c
+# ╟─95544938-4319-11eb-339b-e9349790aea3
+# ╠═9009b516-431a-11eb-17ce-998e61d11549
+# ╟─aa8e98e2-4320-11eb-267d-ddd980a07d3f
+# ╠═efb77868-431f-11eb-2173-8bb0a2cf97c9
+# ╟─e33547b0-4319-11eb-1f54-ff0d5101b85c
+# ╠═db2cd3b0-431b-11eb-0635-0d71b8e3ab29
+# ╟─49de5dc2-4328-11eb-3352-57e88bddb934
+# ╟─e88e67b6-4317-11eb-1a9f-3b7864692294
